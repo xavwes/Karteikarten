89eb1a4d6b999a40a7caeef2ab8fce574fb83ec7
xavwes <xaver.westenthanner@googlemail.com>
2015-06-24 13:58:22 +0200

    Register Service programmed

--- karteikarten-server/register.php
+++ karteikarten-server/register.php
@@ -1,1 +1,19 @@
-test
+<?php
+    include('dbConnection.php');
+
+    $connection = connectToDatabase();
+
+    if ($connection->connect_error)
+    {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+
+    echo $username . ' ' . $password;
+
+    $sql = "Insert into users(id, username, password) Values(0, '" . $username . "','" . $password . "')";
+    mysql_query($sql) or die("Anfrage Failed");
+
+?>
