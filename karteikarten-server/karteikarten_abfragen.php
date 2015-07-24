77792391c17f35ce40b6160c0dfdb2f994f9c51e
awallis89 <awallis@alexanders-mbp.speedport_w_724v_typ_a_05011603_00_003>
2015-07-24 13:49:07 +0200

    Karteikarten-Abfrage korrigiert
    returns answer text  instead of id

--- karteikarten-server/karteikarten_abfragen.php
+++ karteikarten-server/karteikarten_abfragen.php
@@ -18,6 +18,7 @@
     $sql = "Select * from karteikarten where user='" . $id . "'";
     $result = mysql_query($sql) or die("Karteikarten-Anfrage failed");
 
+
     $karteikarten_array = array();
     
     if(mysql_num_rows($result) == 0){
@@ -29,7 +30,14 @@
         $karteikarten_array['Karteikarten'] = array();
         while($row = mysql_fetch_array($result)){
             $row_array['frage'] = $row['question'];
-            $row_array['antwort'] = $row['answer'];
+
+            // get the answer from answer id
+            $answerSql = "Select * from answers where id=" . $row['answer'];
+            $res = mysql_query($answerSql) or die("Anfrage auf Answer-Table failed");
+            
+            while($return = mysql_fetch_array($res)){
+                $row_array['antwort'] = $return['answer'];
+            }
 
             array_push($karteikarten_array['Karteikarten'], $row_array); 
         }
