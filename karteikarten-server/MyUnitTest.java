512f0bde0e49c5d49970857b4960f3124e4f63c0
shoelzl <shoelzl@haw-landshut.de>
2015-08-26 10:59:25 +0200

    Delete MyUnitTest.java

--- karteikarten-server/MyUnitTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.*; 
-// by FYICenter.com
-public class HelloTest { 
-    @Test public void testHello() {
-        String message = "Hello World!";
-        Assert.assertEquals(12, message.length());
-    }
-}
