27e17e02fa7079171fa6424516e9f9b73dca785e
shoelzl <shoelzl@haw-landshut.de>
2015-08-27 09:21:59 +0200

    Update HelloTest.java

--- karteikarten-server/HelloTest.java
+++ karteikarten-server/HelloTest.java
@@ -1,5 +1,11 @@
 import org.junit.*;
-import android.util.Log;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
 // by FYICenter.com
 public class HelloTest { 
     @Test public void testHello() {
@@ -8,10 +14,10 @@ public class HelloTest {
     }
     
     @Test
-	public void register(){
+	public void register() throws ClientProtocolException, IOException{
 		// Registrieren service mit neuem Benutzer
 		StringBuilder url_request = new StringBuilder("http://h2467150.stratoserver.net/register.php?registername=newUser&registerpassword=newUserPassword");
-            url_request.append(params[0]);
+			url_request.append(params[0]);
 
             Log.i("url", url_request.toString());
             HttpClient httpClient = new DefaultHttpClient();
@@ -24,16 +30,16 @@ public class HelloTest {
 			
 		// Registrieren service mit bestehendem Benutzer
 		
-		StringBuilder url_request = new StringBuilder("http://h2467150.stratoserver.net/register.php?registername=user1&registerpassword=test");
+		StringBuilder url_request2 = new StringBuilder("http://h2467150.stratoserver.net/register.php?registername=user1&registerpassword=test");
             url_request.append(params[0]);
 
-            Log.i("url", url_request.toString());
-            HttpClient httpClient = new DefaultHttpClient();
+            Log.i("url", url_request2.toString());
+            HttpClient httpClient2 = new DefaultHttpClient();
 
-            HttpGet httpGet = new HttpGet(url_request.toString());
-            HttpResponse getResponse = httpClient.execute(httpGet);
+            HttpGet httpGet2 = new HttpGet(url_request2.toString());
+            HttpResponse getResponse2 = httpClient2.execute(httpGet2);
             
             
-            assertEquals("user"+":"+"exists", getResponse);
+            assertEquals("user"+":"+"exists", getResponse2);
 	}
 }
